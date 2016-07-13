bc7ac564d8a419cc2a1ee17ceab5197ea71b8875
Darren Tsung <darren.tsung@gmail.com>
2016-07-13 16:25:51 -0700

    Add social buttons instead of links

---
+++ sidebar.js
@@ -0,0 +1,15 @@
+document.write('\
+\
+  <div class="sidebar">\
+    <a href="index.html"><b><p class="sidebar-title">Darren Tsung</p></b></a>\
+    <i><p class="sidebar-subtitle">Overly passionate Unity developer</h2></i>\
+    <ul class="sidebar-list">\
+      <a href="https://github.com/DarrenTsung"><img class="social-image" src="./img/social/github.png" alt="Github"></a>\
+      <a href="https://twitter.com/darrentsung"><img class="social-image" src="./img/social/twitter.png" alt="Twitter"></a>\
+      <li><a href="projects.html">Projects</a></li>\
+      <li><a href="writing.html">Writing</a></li>\
+      <li><a href="res/most_recent_resume.pdf">Resume</a></li>\
+    </ul>\
+  </div>\
+\
+');
