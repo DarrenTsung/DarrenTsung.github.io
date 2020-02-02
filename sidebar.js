62dbcf7d7a59fc5eab12270ab700ae84d4e85f09
Darren Tsung <darren@onesignal.com>
2020-02-02 10:59:01 -0800

    Replace index.html with projects page, add blurb about self at top

--- sidebar.js
+++ sidebar.js
@@ -3,15 +3,7 @@ document.write('\
   <div class="sidebar">\
     <a href="index.html">\
     <img src="./img/social/profile-photo.png" alt="9 out of 10">\
-    <b><p class="sidebar-title">Darren Tsung</p></b>\
-    <\a>\
-    <i><p class="sidebar-subtitle">Backend Engineer</h2></i>\
-    <ul class="sidebar-list">\
-      <a href="https://github.com/DarrenTsung"><img class="social-image" src="./img/social/github.png" alt="Github"></a>\
-      <a href="https://twitter.com/darrentsung"><img class="social-image" src="./img/social/twitter.png" alt="Twitter"></a>\
-      <li><a href="projects.html">Projects</a></li>\
-      <li><a href="res/darrentsung_resume.pdf">Resume</a></li>\
-    </ul>\
-  </div>\
+    </a>\
+</div >\
 \
 ');
