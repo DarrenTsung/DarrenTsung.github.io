e64c013569db94fd2f4a05cd4e86bf7d86f7b0e4
Darren Tsung <darren@onesignal.com>
2020-02-02 10:01:45 -0800

    Remove writing.html and link profile photo to index

--- sidebar.js
+++ sidebar.js
@@ -1,14 +1,15 @@
 document.write('\
 \
   <div class="sidebar">\
+    <a href="index.html">\
     <img src="./img/social/profile-photo.png" alt="9 out of 10">\
-    <a href="index.html"><b><p class="sidebar-title">Darren Tsung</p></b></a>\
+    <b><p class="sidebar-title">Darren Tsung</p></b>\
+    <\a>\
     <i><p class="sidebar-subtitle">Backend Engineer</h2></i>\
     <ul class="sidebar-list">\
       <a href="https://github.com/DarrenTsung"><img class="social-image" src="./img/social/github.png" alt="Github"></a>\
       <a href="https://twitter.com/darrentsung"><img class="social-image" src="./img/social/twitter.png" alt="Twitter"></a>\
       <li><a href="projects.html">Projects</a></li>\
-      <li><a href="writing.html">Writing</a></li>\
       <li><a href="res/darrentsung_resume.pdf">Resume</a></li>\
     </ul>\
   </div>\
