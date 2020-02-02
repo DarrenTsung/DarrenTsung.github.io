dd2c28663e9a96f7c51bd2eceb4be36fa918b752
Darren Tsung <darren@onesignal.com>
2020-02-02 11:16:34 -0800

    Add newer profile picture

--- sidebar.js
+++ sidebar.js
@@ -2,7 +2,7 @@ document.write('\
 \
   <div class="sidebar">\
     <a href="index.html">\
-    <img src="./img/social/profile-photo.png" alt="9 out of 10">\
+    <img id="profile-pic" src="./img/social/ProfilePicture.jpg" alt="9 out of 10">\
     </a>\
 </div >\
 \
